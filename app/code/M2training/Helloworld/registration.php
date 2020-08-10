b8ab17b0c2cd509e193714fed79b724879430b5d
U-CTS\825544 <825544@LTO91572.cts.com>
2020-08-11 01:29:55 +0530

    reg.php fixed

--- app/code/M2training/Helloworld/registration.php
+++ app/code/M2training/Helloworld/registration.php
@@ -1,7 +1,6 @@
 <?php
-
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'M2training_Helloworld',
-    __DIR__
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'M2training_HelloWorld',
+	__DIR__
 );
