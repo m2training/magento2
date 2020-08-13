cf34e877093885f776b0428610bd5ae1af0846f9
U-CTS\825544 <825544@LTO91572.cts.com>
2020-08-13 19:22:08 +0530

    fix1

--- app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
@@ -10,6 +10,6 @@ class Test extends \Magento\Framework\App\Action\Action
 		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
 		$this->_eventManager->dispatch('mageplaza_helloworld_display_text', ['mp_text' => $textDisplay]);
 		echo $textDisplay->getText();
-		exit;
+		//exit;
 	}
 }
