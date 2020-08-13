981f3b8120c79a1240c60397498169ed17acb92f
U-CTS\825544 <825544@LTO91572.cts.com>
2020-08-13 12:21:27 +0530

    Preference Example

--- app/code/Mageplaza/HelloWorld/Block/ChangeText.php
+++ app/code/Mageplaza/HelloWorld/Block/ChangeText.php
@@ -1,16 +1,7 @@
 <?php
-/**
- *
- * @package     magento2
- * @author      Cognizant Technologies
- * @license     https://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
- * @link        https://www.Cognizant.com/
- */
-
 namespace Mageplaza\HelloWorld\Block;
 
 
-use Cognizant\HelloWorld\Block\Hello;
 
 class ChangeText extends Cognizant\HelloWorld\Block\Hello
 {
