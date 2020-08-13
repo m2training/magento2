e3f086d97882eb8cdf1109bafb8c99d87702644e
U-CTS\825544 <825544@LTO91572.cts.com>
2020-08-13 08:58:38 +0530

    Pulgin Example fix

--- app/code/Mageplaza/HelloWorld/Plugin/ExamplePlugin.php
+++ app/code/Mageplaza/HelloWorld/Plugin/ExamplePlugin.php
@@ -14,25 +14,6 @@ class ExamplePlugin
 	}
 
 
-	public function aroundGetTitle(\Mageplaza\HelloWorld\Controller\Index\Example $subject, callable $proceed)
-	{
-
-		echo __METHOD__ . " - Before proceed() </br>";
-		 $result = $proceed();
-		echo __METHOD__ . " - After proceed() </br>";
-
-
-		return $result;
-	}
-	
-	public function afterGetTitle(\Mageplaza\HelloWorld\Controller\Index\Example $subject, $result)
-	{
-
-		echo __METHOD__ . "</br>";
-
-		return '<h1>'. $result . 'Mageplaza.com' .'</h1>';
-
-	}
 	
 	
 }
