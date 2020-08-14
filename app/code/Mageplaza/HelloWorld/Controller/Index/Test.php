f19f5654d056b9603240295e186c13ae6a519c1b
U-CTS\825544 <825544@LTO91572.cts.com>
2020-08-14 11:53:54 +0530

    EO

--- app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
@@ -7,7 +7,7 @@ class Test extends \Magento\Framework\App\Action\Action
 
 	public function execute()
 	{
-		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Test event Observe'));
 		$this->_eventManager->dispatch('mageplaza_helloworld_display_text', ['mp_text' => $textDisplay]);
 		echo $textDisplay->getText();
 		//exit;
