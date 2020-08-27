698777c6be76888ae1846e0583466ac6ec82b241
U-CTS\825544 <825544@LTO91572.cts.com>
2020-08-27 10:03:08 +0530

    data

--- app/code/Mageplaza/HelloWorld/Model/ProductImage/Status.php
+++ app/code/Mageplaza/HelloWorld/Model/ProductImage/Status.php
@@ -6,7 +6,7 @@ class Status implements \Magento\Framework\Option\ArrayInterface
     public function toOptionArray()
     {
         $options = [];
-        $options[] = ['label' => 'One', 'value' => 'post name -1'];
+        $options[] = ['label' => 'One', 'value' => 'post-name-1'];
         $options[] = ['label' => 'Two', 'value' => 'two'];
         $options[] = ['label' => 'Three', 'value' => 'three'];
         $options[] = ['label' => 'Four', 'value' => 'four'];
