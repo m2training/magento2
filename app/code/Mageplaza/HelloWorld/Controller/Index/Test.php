1ade5e6bd5e1e6492cf89bed64ed67ecc90348e7
U-CTS\825544 <825544@LTO91572.cts.com>
2020-09-07 16:19:51 +0530

    cs

--- app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
@@ -5,6 +5,16 @@ namespace Mageplaza\HelloWorld\Controller\Index;
 class Test extends \Magento\Framework\App\Action\Action
 {
 
+	protected $_dataHelper;
+
+	public function __construct(
+		\Mageplaza\HelloWorld\Helper\Data $dataHelper
+		
+	)
+	{		
+		$this->_dataHelper = $dataHelper;
+	}
+
 	public function execute()
 	{
 		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Test event Observe'));
