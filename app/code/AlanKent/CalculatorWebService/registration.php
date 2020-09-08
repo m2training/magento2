dcac117b4a3a6692427d355225277b909256e260
U-CTS\825544 <825544@LTO91572.cts.com>
2020-09-08 07:04:50 +0530

    Api

--- app/code/AlanKent/CalculatorWebService/registration.php
+++ app/code/AlanKent/CalculatorWebService/registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'AlenKent_CalculatorWebService',
+        'AlanKent_CalculatorWebService',
         __DIR__
     );
