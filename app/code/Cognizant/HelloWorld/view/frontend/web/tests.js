4e075a2bbc0a408f505e924dbaaa23d9e0f6d655
U-CTS\825544 <825544@LTO91572.cts.com>
2020-09-16 16:14:20 +0530

    test

--- app/code/Cognizant/HelloWorld/view/frontend/web/tests.js
+++ app/code/Cognizant/HelloWorld/view/frontend/web/tests.js
@@ -2,8 +2,8 @@ define(function() {
     'use strict';
 
     var mageJsComponent = function(config, node){
-        console.log("test module javascript file loading through Requirejs");
-        console.log(config);
+        //console.log("test module javascript file loading through Requirejs");
+       //console.log(config);
         console.log(node); 
     }
 
