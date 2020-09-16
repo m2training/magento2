adeef856148d7d0e9ea020251201d5cffec0caa2
U-CTS\825544 <825544@LTO91572.cts.com>
2020-09-16 14:33:14 +0530

    test

--- app/code/Cognizant/HelloWorld/view/frontend/web/test.js
+++ app/code/Cognizant/HelloWorld/view/frontend/web/test.js
@@ -1,8 +1,8 @@
 define(function() {
     'use strict';
 
-    return function(){
-        console.log("test module javascript file loading through Requirejs");
+    return function(config){
+        console.log("test module javascript file loading through Requirejs", config);
     }
 
 })
